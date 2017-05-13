ef3da75828285de0f77f1dc69c34ea9cd47ce4df
geekymoose <constantin.masson@umontreal.ca>
2017-05-13 13:57:46 -0400

    Beta version for camera follow 2 targets

---
+++ Assets/Scripts/Camera/cameraFollow2.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow2 : MonoBehaviour{
+    // ------------------------------------------------------------------------
+    // Attributes
+    // ------------------------------------------------------------------------
+    public Transform target1;
+    public Transform target2;
+    public float smoothZoom = 0.5f;
+    public float edgeDistance = 4f;
+    public float zoomMinDistance = 1f;
+
+    private Camera camera;
+    private Vector3 averagePosition;
+    private Vector3 offset;
+    private float currentVelocity;
+
+
+    // ------------------------------------------------------------------------
+    // Unity functions
+    // ------------------------------------------------------------------------
+    // Use this for initialization
+    void Start(){
+        this.camera = GetComponentInChildren<Camera>();
+        this.averagePosition = this.calculateAveragePosition();
+        this.offset = this.transform.position - this.averagePosition;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+        // Move
+        this.averagePosition = this.calculateAveragePosition();
+        Vector3 newposition = this.averagePosition + offset;
+        this.transform.position = Vector3.Lerp(this.transform.position, newposition, Time.deltaTime * this.smoothZoom);
+        //Update zoom
+        float newZoomSize = this.calculateZoomSize();
+        this.camera.orthographicSize = Mathf.SmoothDamp(this.camera.orthographicSize, newZoomSize, ref currentVelocity, smoothZoom);
+    }
+
+
+    // ------------------------------------------------------------------------
+    // Functions
+    // ------------------------------------------------------------------------
+    private Vector3 calculateAveragePosition(){
+        float x = (target1.position.x + target2.position.x) / 2;
+        //float y = (target1.position.y + target2.position.y)/2;
+        float z = (target1.position.z + target2.position.z) / 2;
+        Vector3 averagePos = new Vector3(x, 0, z);
+        Debug.DrawRay(averagePos, Vector3.up, Color.red, 1); //DEBUG
+        return averagePos;
+    }
+
+    private float calculateZoomSize(){
+        // Convert vectors to use local position using camera position as reference
+        Vector3 camLocalPos = this.camera.transform.InverseTransformPoint(this.averagePosition);
+
+        // Local target positions from camera
+        Vector3 target1LocalPos = this.camera.transform.InverseTransformPoint(this.target1.position);
+        Vector3 target2LocalPos = this.camera.transform.InverseTransformPoint(this.target2.position);
+
+        // Local targets positions from middle point
+        Vector3 t1LocalPosToPoint = target1LocalPos - camLocalPos;
+        Vector3 t2LocalPostToPoint = target2LocalPos - camLocalPos;
+
+        //Get the max x
+        float size = 0f;
+
+        // Calculate size for z axes
+        size = Mathf.Max(size, Mathf.Abs(t1LocalPosToPoint.y));
+        size = Mathf.Max(size, Mathf.Abs(t2LocalPostToPoint.y));
+
+        // Calculate size for x axes
+        size = Mathf.Max(size, Mathf.Abs(t1LocalPosToPoint.x) / this.camera.aspect);
+        size = Mathf.Max(size, Mathf.Abs(t2LocalPostToPoint.x) / this.camera.aspect);
+
+        size += this.edgeDistance;
+        size = Mathf.Max(size, this.zoomMinDistance);
+        Debug.Log("SIZE: " + size);
+        return size;
+    }
+}
---
+++ Assets/Scripts/Movement/dbgMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dbgMove : MonoBehaviour {
+    private Rigidbody rg;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        this.rg = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        this.handleMovement(x, y, 0);
+    }
+
+    private void handleMovement(float x, float y, float z){
+        x = x * this.speed;
+        y = y * this.speed;
+        z = z * this.speed;
+        Vector3 newv = new Vector3(x, y, z);
+        this.rg.velocity = newv;
+    }
+}
