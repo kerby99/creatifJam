0ecad77cb0e8323be201c9bdf6c998e9e5fc87cb
unknown <nadjam@326-C2.nad.qc.ca>
2017-05-13 12:39:17 -0400

    Player2 is moving

---
+++ Assets/Scripts/Movement/walkP2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class walkP2 : MonoBehaviour {
+    public float speed = 0f;
+    private Rigidbody2D rigid;
+
+
+    // Use this for initialization
+    void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float moveH = Input.GetAxis("HPlayer2") * speed;
+        moveH *= Time.deltaTime;
+        rigid.velocity = new Vector2(moveH, rigid.velocity.y);
+    }
+}
