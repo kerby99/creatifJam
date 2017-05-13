961481c3618814130d7e4c921404370b6d873468
geekymoose <constantin.masson@umontreal.ca>
2017-05-13 14:24:32 -0400

    Camera follow 2 target - Working version

--- Assets/Scripts/Camera/cameraFollow2.cs
+++ Assets/Scripts/Camera/cameraFollow2.cs
@@ -5,16 +5,16 @@ public class cameraFollow2 : MonoBehaviour{
     // ------------------------------------------------------------------------
     // Attributes
     // ------------------------------------------------------------------------
-    public Transform target1;
-    public Transform target2;
-    public float smoothZoom = 0.5f;
-    public float edgeDistance = 4f;
-    public float zoomMinDistance = 1f;
+    public Transform    target1;
+    public Transform    target2;
+    public float        smoothZoom          = 0.5f;
+    public float        edgeDistance        = 4f;
+    public float        zoomMinDistance     = 1f;
 
-    private Camera camera;
-    private Vector3 averagePosition;
-    private Vector3 offset;
-    private float currentVelocity;
+    private Camera      camera;
+    private Vector2     averagePosition;
+    private Vector2     velocity2D;
+    private float       velocity;
 
 
     // ------------------------------------------------------------------------
@@ -22,36 +22,38 @@ public class cameraFollow2 : MonoBehaviour{
     // ------------------------------------------------------------------------
     // Use this for initialization
     void Start(){
-        this.camera = GetComponentInChildren<Camera>();
-        this.averagePosition = this.calculateAveragePosition();
-        this.offset = this.transform.position - this.averagePosition;
+        this.camera             = GetComponentInChildren<Camera>();
+        this.averagePosition    = this.calculateAveragePosition();
     }
 
     // Update is called once per frame
     void FixedUpdate(){
-        // Move
-        this.averagePosition = this.calculateAveragePosition();
-        Vector3 newposition = this.averagePosition + offset;
-        this.transform.position = Vector3.Lerp(this.transform.position, newposition, Time.deltaTime * this.smoothZoom);
+        // Move camera position
+        Vector2 newposition = this.calculateAveragePosition();
+        this.transform.position = Vector2.SmoothDamp(this.transform.position, newposition, ref velocity2D, smoothZoom);
+        this.transform.position += Vector3.back; // Just used otherwise camera is behind map
+
         //Update zoom
         float newZoomSize = this.calculateZoomSize();
-        this.camera.orthographicSize = Mathf.SmoothDamp(this.camera.orthographicSize, newZoomSize, ref currentVelocity, smoothZoom);
+        this.camera.orthographicSize = Mathf.SmoothDamp(this.camera.orthographicSize, newZoomSize, ref velocity, smoothZoom);
     }
 
 
     // ------------------------------------------------------------------------
     // Functions
     // ------------------------------------------------------------------------
-    private Vector3 calculateAveragePosition(){
+    private Vector2 calculateAveragePosition(){
         float x = (target1.position.x + target2.position.x) / 2;
-        //float y = (target1.position.y + target2.position.y)/2;
-        float z = (target1.position.z + target2.position.z) / 2;
-        Vector3 averagePos = new Vector3(x, 0, z);
-        Debug.DrawRay(averagePos, Vector3.up, Color.red, 1); //DEBUG
+        float y = (target1.position.y + target2.position.y) / 2;
+        Vector2 averagePos = new Vector2(x, y);
+        Debug.DrawRay(averagePos, Vector2.up, Color.red, 1); //DEBUG
         return averagePos;
     }
 
     private float calculateZoomSize(){
+        // DEV NOTE: this is actually usefull for 3D but can be way simplified for 2D
+        // See http://answers.unity3d.com/questions/674225/2d-camera-to-follow-two-players.html for 2D example
+
         // Convert vectors to use local position using camera position as reference
         Vector3 camLocalPos = this.camera.transform.InverseTransformPoint(this.averagePosition);
 
